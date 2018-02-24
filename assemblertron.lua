e41e58b67a8c1f46483590597f4daaa7db68dc1f
Crazylemon64 <tlrgue17@gmail.com>
2018-02-24 13:35:50 -0800

    Assemblertron mk 2

--- assemblertron.lua
+++ assemblertron.lua
@@ -4,6 +4,46 @@ local tp = component.transposer
 
 local sides = require("sides")
 
-for i=0,5 do
-  print(i, sides[i], tp.getInventoryName(i), tp.getInventorySize(i))
+local inv_names = 
+{
+  assembler={
+    name="opencomputers:adapter",
+    side=sides.unknown
+  },
+  interface={
+    name="appliedenergistics2:interface",
+    side=sides.unknown
+  }
+}
+
+function registerInventories()
+  for i=0,5 do
+    for _, v in pairs(inv_names) do
+      if v.name == tp.getInventoryName(i) then
+        v.sides = i
+      end
+    end
+  end
 end
+
+function checkInventories()
+  local fail_table = {}
+  for _, v in pairs(inv_names) do
+    if v.sides == sides.unknown then
+      fail_table.insert(v)
+    end
+  end
+  if #fail_table then
+    return false, fail_table
+  end
+  return true
+end
+
+registerInventories()
+local ok,bad_invs = checkInventories()
+
+if not ok then
+  print("Could not find inventories %s":format(serialization.serialize(bad_invs,true)))
+  os.exit()
+end
+print("Inventories found!")
